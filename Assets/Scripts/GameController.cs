b2b06ecd7c7b353b7e0cc42893c672fc82d14617
demonstalker show <demostalker26@gmail.com>
2021-04-25 15:32:40 +0300

    Сцену полигон изменил...

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,34 +1,88 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class GameController : MonoBehaviour
 {
     [Header("Параметры")]
     public float speed;
     [Space]
-    public float move;
+    public float hp;
     [Space]
-    public float move2;
+    public Text hpText;
+    [Space]
+    bool triger;
     [Space]
     [Header("Физика")]
     public Rigidbody2D rb;
-    
+
+    [SerializeField] Joystick joystick;
+    [SerializeField] Joystick joystick2;
+    [SerializeField] GameObject gameObj;
+    [SerializeField] GameObject backobj;
 
 
     void Start()
     {
+        StartCoroutine(Regenerate());
         rb = GetComponent<Rigidbody2D>();
-        
+        hpText.text = ((int)hp).ToString();
     }
 
     void FixedUpdate()
     {
-        //move = Input.GetAxisRaw("Horizontal");
-        rb.AddForce(rb.transform.up * move * speed);
-        //move2 = Input.GetAxisRaw("Vertical");
-        rb.AddForce(rb.transform.right * move2 * speed);
+        if (triger)
+        {
+            UpdateText();
+            TakeDamage();            
+        }
         Controller();
+        JoyController();
+        JoyController2();
     }
+
+    public void JoyController()
+    {
+        if (joystick.Horizontal > 0.1f)
+        {
+            rb.AddForce(-rb.transform.right * speed);
+        }
+        else if (joystick.Horizontal < -0.1f)
+        {
+            rb.AddForce(rb.transform.right * speed);
+        }
+        else if (joystick.Vertical > 0.1f)
+        {
+            rb.AddForce(-rb.transform.up * speed);
+        }
+        else if (joystick.Vertical < -0.1f)
+        {
+            rb.AddForce(rb.transform.up * speed);
+        }
+
+    }
+
+    public void JoyController2()
+    {
+        if (joystick2.Horizontal > 0.1f)
+        {
+            rb.AddForce(-rb.transform.right * speed);
+        }
+        else if (joystick2.Horizontal < -0.1f)
+        {
+            rb.AddForce(rb.transform.right * speed);
+        }
+        else if (joystick2.Vertical > 0.1f)
+        {
+            rb.AddForce(-rb.transform.up * speed);
+        }
+        else if (joystick2.Vertical < -0.1f)
+        {
+            rb.AddForce(rb.transform.up * speed);
+        }
+
+    }
+
     void Controller()
     {
         if (Input.GetKey(KeyCode.W))
@@ -54,13 +108,58 @@ public class GameController : MonoBehaviour
         
     }
 
+    void TakeDamage()
+    {
+        hp -= 10 * Time.deltaTime;
+
+        if (hp < 0)
+        {
+            hp = 0;
+            Destroy(gameObject);
+            gameObj.SetActive(true);
+            Time.timeScale = 0f;
+            backobj.SetActive(false);
+        }
+    }
+
+    void UpdateText()
+    {
+        hpText.text = ((int)hp).ToString();
+    }
+
+    IEnumerator Regenerate()
+    {
+        if (hp > 0)
+        {
+            yield return new WaitForSeconds(1);
+            hp += 30 * Time.deltaTime;
+            hpText.text = ((int)hp).ToString();
+        }
+        if (hp > 60)
+            hp = 60;
+        Repeat();
+    }
+
+    void Repeat()
+    {
+        StartCoroutine(Regenerate());
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Tank"))
         {
-            Destroy(gameObject);
-            SceneManager.LoadScene("GameOver3");
+            triger = true;
+            TakeDamage();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tank"))
+        {
+            Destroy(collision.gameObject);
+            triger = false;            
         }
     }
 }
