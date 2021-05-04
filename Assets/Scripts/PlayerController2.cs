e095347a3d3b79ef4fc50c29a0d99d53ae5cc1c8
demonstalker show <demostalker26@gmail.com>
2021-05-04 13:58:56 +0300

    Изменения коснулись уровня танковый парад

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController2 : MonoBehaviour
 {
@@ -7,56 +8,159 @@ public class PlayerController2 : MonoBehaviour
     [Header("Параметры")]
     public float speed;
     [Space]
-    public float move;
+    public float hp;
     [Space]
-    public float move2;
+    public Text hpText;
+    [Space]
+    bool triger;
     [Space]
     [Header("Физика")]
     public Rigidbody2D rb;
+    [Space]
+    [Header("Анимация")]
+    public Animator animat;
+
+    [SerializeField] Joystick joystick;
+    [SerializeField] Joystick joystick2;
+    [SerializeField] GameObject gameObj;
+    [SerializeField] GameObject backobj;
 
     void Start()
     {
+        StartCoroutine(Regenerate());
         rb = GetComponent<Rigidbody2D>();
+        hpText.text = ((int)hp).ToString();
+        animat = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        //move = Input.GetAxisRaw("Horizontal");
-        rb.AddForce(rb.transform.right * move * speed);
-        //move2 = Input.GetAxisRaw("Vertical");
-        rb.AddForce(rb.transform.up * move2 * speed);
+        if (triger)
+        {
+            UpdateText();
+            TakeDamage();
+        }
         Controller();
+        JoyController();
+        JoyController2();
+    }
+
+    public void JoyController()
+    {
+        if (joystick.Horizontal > 0.1f)
+        {
+            rb.AddForce(-rb.transform.right * speed);
+        }
+        else if (joystick.Horizontal < -0.1f)
+        {
+            rb.AddForce(rb.transform.right * speed);
+        }
+        else if (joystick.Vertical > 0.1f)
+        {
+            rb.AddForce(-rb.transform.up * speed);
+        }
+        else if (joystick.Vertical < -0.1f)
+        {
+            rb.AddForce(rb.transform.up * speed);
+        }
+
+    }
+
+    public void JoyController2()
+    {
+        if (joystick2.Horizontal > 0.1f)
+        {
+            rb.AddForce(-rb.transform.right * speed);
+        }
+        else if (joystick2.Horizontal < -0.1f)
+        {
+            rb.AddForce(rb.transform.right * speed);
+        }
+        else if (joystick2.Vertical > 0.1f)
+        {
+            rb.AddForce(-rb.transform.up * speed);
+        }
+        else if (joystick2.Vertical < -0.1f)
+        {
+            rb.AddForce(rb.transform.up * speed);
+        }
+
     }
     void Controller()
     {
         if (Input.GetKey(KeyCode.W))
-        {
-            
+        {            
             rb.AddForce(-rb.transform.up * speed);
         }
         if (Input.GetKey(KeyCode.S))
-        {
-            
+        {            
             rb.AddForce(rb.transform.up * speed);
         }
         if (Input.GetKey(KeyCode.A))
-        {
-            
+        {            
             rb.AddForce(rb.transform.right * speed);
         }
         if (Input.GetKey(KeyCode.D))
-        {
-            
+        {            
             rb.AddForce(-rb.transform.right * speed);
         }
         
     }
+
+    void TakeDamage()
+    {
+        hp -= 10 * Time.deltaTime;
+
+        if (hp < 0)
+        {
+            hp = 0;
+            Destroy(gameObject);
+            gameObj.SetActive(true);
+            Time.timeScale = 0f;
+            backobj.SetActive(false);
+        }
+    }
+
+    void UpdateText()
+    {
+        hpText.text = ((int)hp).ToString();
+    }
+
+    IEnumerator Regenerate()
+    {
+        if (hp > 0)
+        {
+            yield return new WaitForSeconds(1);
+            hp += 30 * Time.deltaTime;
+            hpText.text = ((int)hp).ToString();
+        }
+        if (hp > 60)
+            hp = 60;
+        Repeat();
+    }
+
+    void Repeat()
+    {
+        StartCoroutine(Regenerate());
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Tank"))
         {
-            Destroy(gameObject);
-            SceneManager.LoadScene("GameOver3");
+            triger = true;
+            TakeDamage();
+            animat.SetBool("EnemyDamage", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tank"))
+        {
+            Destroy(collision.gameObject);
+            triger = false;
+            animat.SetBool("EnemyDamage", false);
         }
     }
 }
